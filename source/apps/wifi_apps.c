69964b63c87bd61db5868b5ff66f91b04e18ff81
sshah500 <Sachinkumar_Shah@comcast.com>
2025-12-18 15:17:24 +0530

    change name to  multiAp-test

--- source/apps/wifi_apps.c
+++ source/apps/wifi_apps.c
@@ -470,7 +470,7 @@ wifi_app_descriptor_t app_desc[] = {
          wifi_app_inst_multiap, 0,
          wifi_event_type_hal_ind | wifi_event_type_exec | wifi_event_type_webconfig,
          true, true,
-         "MultiAp",
+         "MultiAp-test",
          multiap_init, multiap_event, multiap_deinit,
          NULL, NULL
 },
